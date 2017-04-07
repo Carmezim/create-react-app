b29c4debce26f2f53bbc58594525423cb3087b24
Carmezim <carmezim.filho@gmail.com>
2017-04-07 17:20:57 +0100

    configuring scripts for SASS

--- packages/react-scripts/scripts/init.js
+++ packages/react-scripts/scripts/init.js
@@ -43,10 +43,13 @@ module.exports = function(
 
   // Setup the script rules
   appPackage.scripts = {
-    start: 'react-scripts start',
-    build: 'react-scripts build',
+    start-js: "react-scripts start",
+    start: 'npm-run=all -p watch-css start-js',
+    build: 'npm run build-css && react-scripts build',
     test: 'react-scripts test --env=jsdom',
     eject: 'react-scripts eject',
+    build-css: 'node-sass src/ -o src/',
+    watch-css: 'npm run build-css && node-sass src/ -o src/ --watch --recursive',
   };
 
   fs.writeFileSync(
